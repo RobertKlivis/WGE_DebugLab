34b9ed9890437fd2fba0465c9f9685f668712b09
RobertKlivis <roberts.klivis@gmail.com>
2019-02-18 23:03:29 +0000

    CameraShake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
+public class CameraShake : MonoBehaviour
+{
+    private float maxXOffset;
+    private float maxYOffset;
+    private float maxZOffset;
+    private float xOffset;
+    private float yOffset;
+    private float zOffset;
 
-    public Camera playerCamera;
+    // Use this for initialization
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
+    // Update is called once per frame
+    void Update() {
 
+        maxXOffset = -200.0f;
+        maxYOffset = 50.0f;
+        maxZOffset = -50.0f;
 
-	}
+        xOffset = Random.Range(0.0f, maxXOffset);
+        yOffset = Random.Range(0.0f, maxYOffset);
+        zOffset = Random.Range(0.0f, maxZOffset);
+
+        transform.position = new Vector3(xOffset, yOffset, zOffset);
+
+    }
 }
